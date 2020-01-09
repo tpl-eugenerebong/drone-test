0282114286fb75b7e08549b26c6aa041b4e029ed
tpl-eugenerebong <eugene261997@gmail.com>
2020-01-09 12:00:01 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to BE SUCCESSSSFULLL.
+          or be a banana
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank" 
           rel="noopener noreferrer"
-          
+
         >
           Learn React
         </a>
